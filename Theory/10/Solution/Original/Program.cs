54874b7c55e0bb2c7c82710cd3d77e76faddd66d
veselin465 <33176441+veselin465@users.noreply.github.com>
2020-06-08 16:38:57 +0300

    Added a solution to practical problem №10

---
+++ Theory/10/Solution/Original/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        var instruction = new Instructions();
+        string code = Console.ReadLine();
+        while (code != "END")
+        {
+            //Console.WriteLine(instruction.Exec(code));
+            Console.WriteLine(instruction.Execute(code));
+            code = Console.ReadLine();
+        }
+    }
+}
---
+++ Theory/10/Solution/Solution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        var instruction = new Instructions();
+        string code = Console.ReadLine();
+        while (code != "END")
+        {
+            Console.WriteLine(instruction.Execute(code));
+            code = Console.ReadLine();
+        }
+    }
+}
