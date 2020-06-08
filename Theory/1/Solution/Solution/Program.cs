18ec1b42bd6ce4a6dfaa3c6071f771bfa365f1e5
veselin465 <33176441+veselin465@users.noreply.github.com>
2020-06-08 16:37:28 +0300

    Added a solution to practical problem №1

---
+++ Theory/1/Solution/Solution/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ORIGINAL CODE
+             
+            n = int.Parse(Console.ReadLine);
+            for (a = 1; a < 10 a++)
+                for (int b = 1; b < 10; b++)
+                    for (int c = 1; c < 10; c++)
+                        if ((n % a != 0) || &(n % b == 0) && (n % c = 0))
+                        
+             */
+
+
+
+            /// Грозен, ама работещ вариант
+
+            int n = int.Parse(Console.ReadLine());
+            for (int a = 1; a < 10; a++)
+                for (int b = 1; b < 10; b++)
+                    for (int c = 1; c < 10; c++)
+                        for (int d = 1; d < 10; d++)
+                            if ((n % a == 0) && (n % b == 0) && (n % c == 0) && (n % d == 0))
+                            {
+                                Console.Write(a * 1000 + b * 100 + c * 10 + d);
+                                Console.Write(" ");
+                            }
+
+
+            /// Добре оформен и съобразен вариант
+
+            /*
+            
+            bool isItTheFirstFound = true;
+            int n = int.Parse(Console.ReadLine());
+
+            for (int a = 1; a < 10; a++)
+            {
+                for (int b = 1; b < 10; b++)
+                {
+                    for (int c = 1; c < 10; c++)
+                    {
+                        for (int d = 1; d < 10; d++)
+                        {
+                            if ((n % a == 0) && (n % b == 0) && (n % c == 0) && (n % d == 0))
+                            {
+
+                                if (!isItTheFirstFound)
+                                {
+                                    Console.Write(" ");
+                                }
+                                Console.Write(a * 1000 + b * 100 + c * 10 + d);
+
+                                isItTheFirstFound = false;
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            */
+
+        }
+    }
+}
